5b5411b6f55c382161dcf59290b30b221ebacc4a
Nurulloh <140059040+abdurashidovnurulloh@users.noreply.github.com>
2024-03-31 12:17:48 +0500

    Add files via upload

---
+++ zsh/c/my_string.c
@@ -0,0 +1,15 @@
+#include "../h/my_string.h"
+
+short spliting(char *str, const char *delimeter, char **array) {
+    char *token;
+    str[strlen(str) - 1] = '\0';
+    short count = 0;
+    token = strtok(str, delimeter);
+    while (token != NULL)
+    {
+        array[count++] = strdup(token);
+        token = strtok(NULL, delimeter);
+    }
+    array[count] = NULL;
+    return count;
+}
